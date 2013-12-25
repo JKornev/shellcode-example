2c5154686667c3f4758e4a2e06e627c0957bf1b4
Jora Kornev <8bit.dosninja@gmail.com>
2013-12-25 02:57:10 -0800

    asm shellcode added

---
+++ asm-shellcode/loader.c
@@ -0,0 +1,49 @@
+//Loader.c
+#include <Windows.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	HANDLE hfile;
+	unsigned int size, readed;
+	char *pbuf;
+	void *pspace;
+
+	if (argc < 2) {
+		printf("Error, arguments missmatch\n");
+		return 1;
+	}
+
+	hfile = CreateFileA(argv[1], GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+	if (hfile == INVALID_HANDLE_VALUE) {
+		printf("Error, can't open shellcode '%s' (code %d)\n", argv[1], GetLastError());
+		return 2;
+	}
+
+	size = GetFileSize(hfile, NULL);
+	if (!size) {
+		printf("Error, file is empty\n");
+		return 3;
+	}
+
+	pbuf = (char *)malloc(size);
+
+	if (!ReadFile(hfile, pbuf, size, (LPDWORD)&readed, NULL)) {
+		printf("Error, can't read shellcode data\n");
+		free(pbuf); return 4;
+	}
+
+	pspace = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	if (!pspace) {
+		printf("Error, can't allocate virtual page\n");
+		free(pbuf); return 5;
+	}
+
+	memcpy(pspace, pbuf, size);
+
+	((void (*)())pspace)();//call shellcode
+
+	VirtualFree(pspace, size, MEM_DECOMMIT);
+	free(pbuf);
+	return 0;
+}
